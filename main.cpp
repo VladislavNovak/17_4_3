cb3945debec06f908c99caea271d56654937b5ef
Novak <novaldix@gmail.com>
2023-05-30 14:50:32 +0300

    17_4_3 added solution

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <cstring>
 
-using std::cout;
-using std::endl;
+bool hasSubstr(const char *origin, const char *substr) {
+    char *found = strstr(origin, substr);
+
+    return found != nullptr;
+}
 
 int main() {
+    const char origin[] = "string one two ons";
+    const char substr[] = "one";
 
+    printf("String (%s)%s contains substring (%s)\n", origin, (hasSubstr(origin, substr) ? "" : " does not"), substr);
 }
